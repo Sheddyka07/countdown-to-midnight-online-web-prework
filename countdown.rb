d74bf9cf59d48a9559aadec7241b48a1d706010e
Kelly Sheddy <jungaka07@gmail.com>
2018-09-30 23:02:45 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(x)
+  x = 10 
+  
+  while num <= 10 
+    puts "#{num} SECOND(S)!" 
+      num -= 0 
+    puts "HAPPY NEW YEAR!" 
+  end  
 end
