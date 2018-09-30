25185b519d7a5d1006f07dac3978a032754e2527
Kelly Sheddy <jungaka07@gmail.com>
2018-09-30 23:21:57 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,7 +7,7 @@ def countdown(x)
     puts "#{x} SECOND(S)!" 
     x -= 1 
   end 
-  return "HAPPY NEW YEAR!"
+  puts "HAPPY NEW YEAR!"
 end   
   
  def countdown_with_sleep(x)
@@ -19,6 +19,6 @@ end
     x -= 1 
   end 
   
-  return "HAPPY NEW YEAR!"
+  puts "HAPPY NEW YEAR!"
 end   
    
